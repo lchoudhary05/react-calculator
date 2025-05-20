bc2f37926bf14cf33072c12f15440c2933764563
lchoudhary05 <mails4lavesh@gmail.com>
2025-05-20 11:16:37 -0700

    test

---
+++ src/Calculator.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function add(a,b){
+    return a+b
+}
+
+function subtract(a,b){
+    return a-b
+}
+
+function multiply(a,b){
+    return a*b
+}
+
+function divide(a,b){
+    return a/b
+}
+
+export {add,subtract,divide,multiply}
