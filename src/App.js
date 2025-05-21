831a1d499139ccfd9a827c6dc8524feb50890aa2
lchoudhary05 <mails4lavesh@gmail.com>
2025-05-21 00:42:09 -0700

    make more interactive

--- src/App.js
+++ src/App.js
@@ -2,21 +2,41 @@ import React, { useState } from 'react'
 import Calculator from './Calculator'
 
 function App(){
-  const[value1, SetValue1] = useState()
-  const[value2, SetValue2] = useState()
-  const[operation, setOperation] = useState()
+  const[value1, SetValue1] = useState('')
+  const[value2, SetValue2] = useState('')
+  const[operation, setOperation] = useState('')
   const[res,setres]= useState(false)
+  const[errorMessage,setErrorMessage]= useState()
 
   const handleValue1Change = (e) =>{
     SetValue1(e.target.value)
+    if(e.target.value===''||value2===''||operation==='')
+      setErrorMessage(true)
+    else{
+      setErrorMessage(false)
+      setres(true)
   }
+    
+  }
+
   function handleValue2Change(e){
     SetValue2(e.target.value)
+    if(value1===''||e.target.value===''||operation==='')
+      setErrorMessage(true)
+    else{
+      setErrorMessage(false)
+      setres(true)
+  }
   }
 
   const handleOperationchange = (e)=>{
     setOperation(e.target.value)
-    setres(true)
+    if(value1===''||value2==='')
+      setErrorMessage(true)
+    else{
+      setErrorMessage(false)
+      setres(true)
+    }
   }
 
 
@@ -30,11 +50,13 @@ function App(){
       <br/>
       <label htmlFor="operation">Select the operation</label>
       <select type= "select" id = 'operation' onChange={handleOperationchange} >
-        <option value="+">Addition</option>
-        <option value="-">Subtraction</option>
-        <option value="*">Multiplication</option>
-        <option value="/">Division</option>
+      <option value="">Select operation</option>
+        <option value="Addition">Addition</option>
+        <option value="Subtraction">Subtraction</option>
+        <option value="Multiplication">Multiplication</option>
+        <option value="Division">Division</option>
       </select>
+      {errorMessage && <p>Please Enter both fields</p>}
       {res &&<Calculator num1={value1} num2={value2} oper={operation}/>}
     </>
   )
--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -4,17 +4,20 @@ function Calculator(props){
     var {num1,num2,oper} = props
     var result = NaN
     switch(oper){
-        case '+':
-            result = num1+num2
+        case 'Addition':
+            result = Number(num1)+Number(num2)
             break;
-        case '-':
+        case 'Subtraction':
             result = num1-num2
             break;
-        case '*':
+        case 'Multiplication':
             result = num1*num2
             break;
-        case '/':
-            result = num1/num2
+        case 'Division':
+            if (Number(num2) === 0)
+                return(<p>The divisor should not be 0</p>)          
+            // result = Math.round(num1/num2).toFixed(2)
+            result = parseFloat(num1/num2).toFixed(2)
             break;
         default:
             result =NaN
